cf3bfc330df823e0de3fb234ceb36ed8a4b4c557
Awais Ahmad <51525833+mrkumar22@users.noreply.github.com>
2020-07-05 00:38:25 +0500

    Update connection.php
    Updated to Medoo Version

--- includes/connection.php
+++ includes/connection.php
@@ -1,8 +1,22 @@
 <?php 
+
+// Using Medoo namespace
+require 'Medoo.php';
+use Medoo\Medoo;
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "wpproject";
+ 
+$database = new Medoo([
+	// required
+	'database_type' => 'mysql',
+	'database_name' => $dbname,
+	'server' => $servername,
+	'username' => $username ,
+	'password' => $password
+]);
 
-    $conn = mysqli_connect($servername,$username,$password,$dbname);
+define ('ROOT_PATH', realpath(dirname(__FILE__)));
+define('BASE_URL', 'http://localhost/realestate/');
 ?>
